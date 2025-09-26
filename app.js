ffa227f80256388ad5ef9ec67505465223848dad
elFiurer <juan.caicedo.oliva@gmail.com>
2025-09-25 21:54:55 -0500

    corrigiendo precios

--- app.js
+++ app.js
@@ -5,21 +5,21 @@ document.addEventListener('DOMContentLoaded', () => {
   // 1. DATOS Y ESTADO DE LA APLICACIÓN
   // ===================================
   const PRODUCTS = [
-    { id: 'quinua-kiwicha', name: 'Quinua y Kiwicha', desc: 'Chocolate en barra al 70% cacao con un crujiente mix de superalimentos andinos.', price: 15.00, img: 'imagen/quinua-kiwicha.jpg' },
-    { id: 'mix-frutas', name: 'Mix de Frutas', desc: 'Una explosión de sabores tropicales en barra con fruta deshidratada y chocolate al 70% cacao.', price: 15.00, img: 'imagen/mix-frutas.jpg' },
-    { id: 'chocolate dark', name: 'Chocolate Dark', desc: 'Deliciosa barra de chocolate oscuro al 70% cacao.', price: 15.00, img: 'imagen/chocolatedark.jpg' },
-    { id: 'arandanos-70', name: 'Arándanos', desc: 'Chocolate en barra al 70% cacao con incrustaciones de arándanos antioxidantes.', price: 15.00, img: 'imagen/arandanos.jpg' },
-    { id: 'cerezas', name: 'Cerezas seleccionadas', desc: 'Barra de chocolate al 70% de cacao fusionado con cerezas deshidratadas de la mejor cosecha.', price: 15.00, img: 'imagen/cereza.jpg' },
-    { id: 'pina', name: 'Piña Tropical', desc: 'En barra con trozos de piña deshidratada que aportan un toque exótico a nuestro chocolate.', price: 15.00, img: 'imagen/pina.jpg' },
-    { id: 'lúcuma', name: 'Lúcumas Clásicas', desc: 'Deliciosos trozos de lúcuma en barra bañados con leche al 50% cacao.', price: 15.00, img: 'imagen/lucuma.jpg' },
-    { id: 'aguaymanto', name: 'Aguaymanto', desc: 'Chocolate en barra con el balance perfecto entre el dulzor y la acidez del aguaymanto deshidratado.', price: 15.00, img: 'imagen/aguaymanto.jpg' },
-    { id: 'almendras y maní', name: 'Almendras y Maní', desc: 'Deliciosa barra de chocolate con leche al 50% cacao, con granos de Almendras y Maní.', price: 15.00, img: 'imagen/almendrasymani.jpg' },
-    { id: 'almendras', name: 'Almendras Clásicas', desc: 'Chocolate en barra con leche al 50% cacao con trozos generosos de almendras tostadas.', price: 15.00, img: 'imagen/almendras.jpg' },
-    { id: 'cafe', name: 'Café de Altura', desc: 'Chocolate en barra con leche al 50% fusionado con granos de café de origen único.', price: 15.00, img: 'imagen/cafe.jpg' },
-    { id: 'maní', name: 'Maní', desc: 'Deliciosa barra de chocolate con leche al 50% cacao, con granos de Maní.', price: 15.00, img: 'imagen/mani.jpg' },
-    { id: 'Bombones de Frambuesa', name: 'Bombones Frambuesa', desc: 'Bombones de frambuesa liofilizados , doble chocolate 70% cacao', price: 18.00, img: 'imagen/frambuesa.jpg' },
-    { id: 'almendras-100g', name: 'Almendras Bañadas', desc: 'Almendras enteras tostadas, cubiertas por chocolate al 70% cacao (100g).', price: 17.00, img: 'imagen/almendras-100.jpg' },
-    { id: 'arandanos-100g', name: 'Arándanos Bañados', desc: 'Arándanos deshidratados sumergidos en nuestro más puro chocolate al 70% cacao (100g).', price: 17.00, img: 'imagen/arandanos-100.jpg' },
+    { id: 'quinua-kiwicha', name: 'Quinua y Kiwicha', desc: 'Chocolate en barra al 70% cacao con un crujiente mix de superalimentos andinos.', price: 15.90, img: 'imagen/quinua-kiwicha.jpg' },
+    { id: 'mix-frutas', name: 'Mix de Frutas', desc: 'Una explosión de sabores tropicales en barra con fruta deshidratada y chocolate al 70% cacao.', price: 15.90, img: 'imagen/mix-frutas.jpg' },
+    { id: 'chocolate dark', name: 'Chocolate Dark', desc: 'Deliciosa barra de chocolate oscuro al 70% cacao.', price: 15.90, img: 'imagen/chocolatedark.jpg' },
+    { id: 'arandanos-70', name: 'Arándanos', desc: 'Chocolate en barra al 70% cacao con incrustaciones de arándanos antioxidantes.', price: 15.90, img: 'imagen/arandanos.jpg' },
+    { id: 'cerezas', name: 'Cerezas seleccionadas', desc: 'Barra de chocolate al 70% de cacao fusionado con cerezas deshidratadas de la mejor cosecha.', price: 15.90, img: 'imagen/cereza.jpg' },
+    { id: 'pina', name: 'Piña Tropical', desc: 'En barra con trozos de piña deshidratada que aportan un toque exótico a nuestro chocolate.', price: 15.90, img: 'imagen/pina.jpg' },
+    { id: 'lúcuma', name: 'Lúcumas Clásicas', desc: 'Deliciosos trozos de lúcuma en barra bañados con leche al 50% cacao.', price: 15.90, img: 'imagen/lucuma.jpg' },
+    { id: 'aguaymanto', name: 'Aguaymanto', desc: 'Chocolate en barra con el balance perfecto entre el dulzor y la acidez del aguaymanto deshidratado.', price: 15.90, img: 'imagen/aguaymanto.jpg' },
+    { id: 'almendras y maní', name: 'Almendras y Maní', desc: 'Deliciosa barra de chocolate con leche al 50% cacao, con granos de Almendras y Maní.', price: 15.90, img: 'imagen/almendrasymani.jpg' },
+    { id: 'almendras', name: 'Almendras Clásicas', desc: 'Chocolate en barra con leche al 50% cacao con trozos generosos de almendras tostadas.', price: 15.90, img: 'imagen/almendras.jpg' },
+    { id: 'cafe', name: 'Café de Altura', desc: 'Chocolate en barra con leche al 50% fusionado con granos de café de origen único.', price: 15.90, img: 'imagen/cafe.jpg' },
+    { id: 'maní', name: 'Maní', desc: 'Deliciosa barra de chocolate con leche al 50% cacao, con granos de Maní.', price: 15.90, img: 'imagen/mani.jpg' },
+    { id: 'Bombones de Frambuesa', name: 'Bombones Frambuesa', desc: 'Bombones de frambuesa liofilizados , doble chocolate 70% cacao', price: 18.90, img: 'imagen/frambuesa.jpg' },
+    { id: 'almendras-100g', name: 'Almendras Bañadas', desc: 'Almendras enteras tostadas, cubiertas por chocolate al 70% cacao (100g).', price: 17.90, img: 'imagen/almendras-100.jpg' },
+    { id: 'arandanos-100g', name: 'Arándanos Bañados', desc: 'Arándanos deshidratados sumergidos en nuestro más puro chocolate al 70% cacao (100g).', price: 17.90, img: 'imagen/arandanos-100.jpg' },
   ];
   // Nueva lista de productos que estarán "pronto disponibles"
   const COMING_SOON_PRODUCTS = ['aguaymanto', 'pina', 'lúcuma', 'almendras y maní'];
